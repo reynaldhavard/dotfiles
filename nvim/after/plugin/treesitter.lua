323742443919e4b8430f2a7cf5acf133192e6688
Reynald Havard <reynald.havard@gmail.com>
2023-03-28 16:18:01 +0100

    fix highlighting, autopairs

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,19 +1,24 @@
-require'nvim-treesitter.configs'.setup {
-  yati = {
-    enable = true,
+require('nvim-treesitter.configs').setup {
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+    yati = {
+        enable = true,
 
-    -- Whether to enable lazy mode (recommend to enable this if bad indent happens frequently)
-    default_lazy = true,
 
-    -- Determine the fallback method used when we cannot calculate indent by tree-sitter
-    --   "auto": fallback to vim auto indent
-    --   "asis": use current indent as-is
-    --   "cindent": see `:h cindent()`
-    -- Or a custom function return the final indent result.
-    default_fallback = "auto"
-  },
-  indent = {
-    enable = false -- disable builtin indent module
-  }
+        -- Whether to enable lazy mode (recommend to enable this if bad indent happens frequently)
+        default_lazy = true,
+
+        -- Determine the fallback method used when we cannot calculate indent by tree-sitter
+        --   "auto": fallback to vim auto indent
+        --   "asis": use current indent as-is
+        --   "cindent": see `:h cindent()`
+        -- Or a custom function return the final indent result.
+        default_fallback = "auto"
+    },
+    indent = {
+        enable = false -- disable builtin indent module
+    }
 }
 
