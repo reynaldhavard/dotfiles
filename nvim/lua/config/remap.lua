73a77dfbb23bcaa93ee83f4e1b7554807290c32b
Reynald Havard <reynald.havard@gmail.com>
2023-03-31 16:41:05 +0100

    update setup, add C-c to exit insert mode in remap

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -1,3 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "-", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>f", ":!black %<CR>")
+vim.keymap.set("i", "<C-c>", "<Esc>")
--- setup.sh
+++ setup.sh
@@ -4,8 +4,7 @@
 rm -rf ~/.config/nvim ~/.bashrc ~/.tmux.conf ~/.zshrc
 
 # Create symlinks
-SCRIPT_DIR=$(dirname $(readlink -f $0))
-ln -s ${SCRIPT_DIR}/nvim ~/.config/nvim
-ln -s ${SCRIPT_DIR}/.bashrc ~/.bashrc
-ln -s ${SCRIPT_DIR}/.tmux.conf ~/.tmux.conf
-ln -s ${SCRIPT_DIR}/.zshrc ~/.zshrc
+ln -s $(pwd)/nvim ~/.config/nvim
+ln -s $(pwd)/.bashrc ~/.bashrc
+ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+ln -s $(pwd)/.zshrc ~/.zshrc
